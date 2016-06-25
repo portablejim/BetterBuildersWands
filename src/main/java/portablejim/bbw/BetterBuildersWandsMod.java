3987504d2970910b5fd9f09d6ed57be8e5e4f44b
James McKay <dcseee-github@yahoo.com.au>
2016-06-25 22:38:48 +1000

    Why oh why @LexManos and @cpw ?!

--- src/main/java/portablejim/bbw/BetterBuildersWandsMod.java
+++ src/main/java/portablejim/bbw/BetterBuildersWandsMod.java
@@ -40,7 +40,7 @@ import java.util.Arrays;
 /**
  * Author: Portablejim
  */
-@Mod(modid = BetterBuildersWandsMod.MODID, acceptedMinecraftVersions = "[1.9.4,1.9.4]")
+@Mod(modid = BetterBuildersWandsMod.MODID, acceptedMinecraftVersions = "[1.9.4,1.9.1337)")
 public class BetterBuildersWandsMod {
     public static final String MODID = "betterbuilderswands";
     public static final String LANGID = "bbw";
