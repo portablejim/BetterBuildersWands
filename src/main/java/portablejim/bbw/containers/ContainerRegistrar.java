d1caa97015fd380a0c62ac5f624e6a1e1800fdf3
James McKay <dcseee-github@yahoo.com.au>
2017-01-04 13:15:08 +1100

    Fix registrar

--- src/main/java/portablejim/bbw/containers/ContainerRegistrar.java
+++ src/main/java/portablejim/bbw/containers/ContainerRegistrar.java
@@ -2,17 +2,17 @@ package portablejim.bbw.containers;
 
 import net.minecraftforge.fml.common.Loader;
 import portablejim.bbw.BetterBuildersWandsMod;
-import portablejim.bbw.containers.handlers.handlerBotania;
-import portablejim.bbw.containers.handlers.handlerCapability;
+import portablejim.bbw.containers.handlers.HandlerBotania;
+import portablejim.bbw.containers.handlers.HandlerCapability;
 
 /**
  * Created by james on 28/12/16.
  */
 public class ContainerRegistrar {
     public static void register() {
-        BetterBuildersWandsMod.instance.containerManager.register(new handlerCapability());
+        BetterBuildersWandsMod.instance.containerManager.register(new HandlerCapability());
         if(Loader.isModLoaded("Botania") || Loader.isModLoaded("botania")) {
-            BetterBuildersWandsMod.instance.containerManager.register(new handlerBotania());
+            BetterBuildersWandsMod.instance.containerManager.register(new HandlerBotania());
         }
     }
 }
